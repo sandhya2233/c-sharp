3a76cda6a797a077cf42b9e3e779e9d6fba058b7
sandhya2233 <121788161+sandhya2233@users.noreply.github.com>
2023-01-25 12:34:53 +0530

    Add files via upload

---
+++ CaLculator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+namespace calculator_c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res;
+            Console.Write("Enter First Number:");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Second Number:");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Symbol (/,+,-,*): ");
+            string symbol = Console.ReadLine(); switch (symbol)
+            {
+                case "+":
+                    res = n1 + n2;
+                    Console.WriteLine("Addition: " + res);
+                    break;
+                case "-":
+                    res = n1 - n2;
+                    Console.WriteLine("Subtraction: " + res);
+                    break;
+                case "*":
+                    res = n1 * n2;
+                    Console.WriteLine("Multiplication: " + res);
+                    break;
+                case "/":
+                    res = n1 / n2;
+                    Console.WriteLine("Division: " + res);
+                    break;
+                default:
+                    Console.WriteLine("Wrong input");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
