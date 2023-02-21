411dd36b5d29dbeae9599c4c9d27a7bef15e9845
sandhya2233 <121788161+sandhya2233@users.noreply.github.com>
2023-02-21 20:52:10 +0530

    Add files via upload

---
+++ ConsoleApp5/Delegatecal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ConsoleApp4
+{
+    public delegate void CalculatingEventHandler(int a, int b);
+    class DelegateCal
+    {
+        public event CalculatingEventHandler Calculating;
+        public void RaiseEvent(int a, int b)
+        {
+            if (Calculating != null)
+            {
+                Calculating(a, b);
+            }
+        }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DelegateCal d = new DelegateCal(); d.Calculating += (a, b) =>
+            {
+                Console.WriteLine("Addition: {0}", a + b);
+            };
+            d.Calculating += (a, b) =>
+            {
+                Console.WriteLine("Subtraction: {0}", a - b);
+            };
+            d.Calculating += (a, b) =>
+            {
+                Console.WriteLine("Multiplication: {0}", a * b);
+            };
+            d.Calculating += (a, b) =>
+            {
+                Console.WriteLine("Division: {0}", a / b);
+            };
+            d.RaiseEvent(60, 20);
+        }
+    }
+}
