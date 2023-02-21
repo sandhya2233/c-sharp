97045a50985c8ad5577ffd453c353fff992434ad
sandhya2233 <121788161+sandhya2233@users.noreply.github.com>
2023-02-21 19:37:45 +0530

    Add files via upload

---
+++ employee management/employee management/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace employee_management
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
