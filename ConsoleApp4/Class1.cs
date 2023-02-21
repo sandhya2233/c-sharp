f0ad24b9b0dd49ac592bc7b4b275b23592ad20da
sandhya2233 <121788161+sandhya2233@users.noreply.github.com>
2023-02-21 20:48:32 +0530

    Add files via upload

---
+++ ConsoleApp4/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace ConsoleApp4
+{
+    public class Calculator
+    {
+        public void Add(int a,int b)
+        {
+            int c = a + b;
+            Console.WriteLine("Addition is:" + c);
+        }
+        public void Mul(int a, int b)
+        {
+            int c = a * b;
+            Console.WriteLine("Multiplication is:" + c);
+        }
+        public void Sub(int a, int b)
+        {
+            int c = a - b;
+            Console.WriteLine("Substraction is:" + c);
+        }
+        public void Div(int a, int b)
+        {
+            int c = a / b;
+            Console.WriteLine("Division is:" + c);
+        }
+            
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    public delegate void calDelegate(int a, int b);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator Cal = new Calculator();
+            calDelegate myDelegate;
+            myDelegate = Cal.Add;
+            myDelegate += Cal.Mul;
+            myDelegate += Cal.Sub;
+            myDelegate += Cal.Div;
+
+            myDelegate.Invoke(9, 4);
+        }
+    }
+}
+
+
+
+
